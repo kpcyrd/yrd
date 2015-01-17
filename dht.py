b34b45bf08eafe980e97448c6729908cb9c7346b
kpcyrd <git@rxv.cc>
2015-01-17 13:40:19 +0000

    Implement nightfall client

---
+++ dht.py
@@ -0,0 +1,23 @@
+import requests
+import utils
+import json
+
+
+class DhtPeer(object):
+    def __init__(self, ip, port, publicKey, password, ts=None, **kwargs):
+        self.ip = ip
+        self.port = port
+        self.publicKey = publicKey
+        self.password = password
+        self.kwargs = kwargs
+
+    def credentialstr(self):
+        return utils.to_credstr(self.ip, self.port, self.publicKey, self.password, **self.kwargs)
+
+
+def request_peers(tracker):
+    for peer in requests.get(tracker).json():
+        try:
+            yield DhtPeer(**peer)
+        except TypeError:
+            pass
