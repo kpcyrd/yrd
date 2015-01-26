d6f29fefc4db140688d75796a45d311c949e7f3f
kpcyrd <git@rxv.cc>
2015-01-26 00:19:18 +0000

    Add debug switch until I have something better

--- cjdns.py
+++ cjdns.py
@@ -23,6 +23,7 @@ class Cjdroute(object):
         res = bdecode(self.s.recv(BUFFER_SIZE))
         if 'error' in res and res['error'] != 'none':
             raise Exception(repr(res))
+        # print(repr(res)) # DEBUG SWITCH
         return res
 
     def _send(self, **kwargs):
