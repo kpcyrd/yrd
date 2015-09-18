d2a491738ac2e841bfc1060900cc17f7286b6b75
kpcyrd <git@rxv.cc>
2015-09-18 05:11:02 +0200

    Move constants into const.py

--- yrd/__init__.py
+++ yrd/__init__.py
@@ -5,26 +5,11 @@ import socket
 from . import xcjdns as cjdns
 from . import cjdns as cj
 from . import utils
+from .const import YRD_FOLDER, YRD_PEERS, CJDROUTE_CONF, CJDROUTE_BIN
 import json
 import time
 import sys
 import os
-
-YRD_FOLDER = os.environ.get('YRD_FOLDER', '/etc/yrd')
-YRD_PEERS = os.path.join(YRD_FOLDER, 'peers.d/')
-
-CJDROUTE_CONF = os.environ.get('CJDROUTE_CONF')
-if not CJDROUTE_CONF:
-    for d in [YRD_FOLDER, '/etc']:
-        path = os.path.join(d, 'cjdroute.conf')
-        try:
-            os.stat(path)
-        except OSError:
-            pass
-        else:
-            break
-    CJDROUTE_CONF = path
-CJDROUTE_BIN = os.environ.get('CJDROUTE_BIN', 'cjdroute')
 
 
 @arg('--attach', help='configure running cjdroute')
