68814205f8cac5e36e8e5a85090578a1d3292766
kpcyrd <git@rxv.cc>
2015-09-18 06:28:08 +0200

    Final cleanup

--- yrd/__init__.py
+++ yrd/__init__.py
@@ -1,26 +1,8 @@
-from argh import arg, dispatch, named, ArghParser
-
-
+from argh import ArghParser, dispatch
 from . import start
-
-from .core import a
-from .core import ping
-from .core import r
-from .core import n
-from .core import top
-from .core import mon
-from .core import uplinks
-from .core import whois
-
-from .peer import auth as peer_auth
-from .peer import add as peer_add
-from .peer import ls as peer_ls
-from .peer import remove as peer_remove
-
-from .nf import get as nf_get
-from .nf import auto as nf_peer
-from .nf import announce as nf_announce
-
+from . import core
+from . import peer
+from . import nf
 from . import wrbt
 
 
@@ -33,6 +15,7 @@ parser.add_commands(wrbt.cmd, namespace='wrbt', title='wrbt implementation')
 
 def main():
     dispatch(parser)
+
 
 if __name__ == '__main__':
     main()
